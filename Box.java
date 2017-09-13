3331e4093640b55bdf70b82ab5c2acbc4571404e
Josh Orndorff <jorndorff@pingry.org>
2017-09-12 20:38:12 -0400

    Completed this as much as I'm going to before class.

--- Box.java
+++ Box.java
@@ -3,22 +3,33 @@ public class Box<T> {
   private T contents;
   private boolean isFull;
 
+  // Make a new empty box
   public Box(){
 
   }
 
+  // Make a new box pre-filled with contents
   public Box(T contents){
     this.contents = contents;
   }
 
+  /**
+   * Check the contents of the box
+   */
   public T getContents(){
     return contents;
   }
 
+  /**
+   * Tell whether the box is full or not
+   */
   public boolean isFull(){
     return isFull;
   }
 
+  /**
+   * Empty out the box, and give back hatever was in it
+   */
   public T empty(){
     T junk = contents;
     contents = null;
@@ -35,9 +46,33 @@ public class Box<T> {
   }
 
   public static void main(String[] args){
+
+    // Make two boxes
     Box<String> stringBox = new Box<String>();
+    //TODO start the second box with contents inside
     Box<Integer> intBox   = new Box<Integer>();
 
+    // View contents (via toString method)
+    System.out.println("The boxes contain: " + stringBox + ", " + intBox);
+
+    //TODO Add something to a box
+
+
+
+    // View contents (via toString method)
+    System.out.println("The boxes contain: " + stringBox + ", " + intBox);
+
+    //TODO Test your peek method
+
+
+    // View contents (via toString method)
+    System.out.println("The boxes contain: " + stringBox + ", " + intBox);
+
+    //TODO Empty a boxes
+
+
+
+    // View contents (via toString method)
     System.out.println("The boxes contain: " + stringBox + ", " + intBox);
   }
 }
--- README.md
+++ README.md
@@ -16,6 +16,7 @@ Todo List
 ---------
 1. Implement all the methods
 2. Update all the javadoc
+3. Implement TODOs in main program
 3. Complete preceding sections of this README
 4. Remove this todo list
-5. (optional) Make a more interesting main program
+5. (optional) Make an even more interesting main program
